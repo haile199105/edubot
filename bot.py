96b19626ee147ab8e4b164407fedd935013e1b84
Haile Shibru <129173186+haile199105@users.noreply.github.com>
2026-03-31 09:01:00 +0300

    Use environment variable for GEMINI_KEY
    Replaced hardcoded GEMINI_KEY with environment variable.

--- bot.py
+++ bot.py
@@ -12,7 +12,8 @@ import requests
 BOT_TOKEN = "8755788296:AAEpumtdZTyIfvKGrl_tn6C2MleogO1LyKA"
 SUPABASE_URL = "https://zecfvwgozgljqpmxfliv.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplY2Z2d2dvemdsanFwbXhmbGl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NDU4NjUsImV4cCI6MjA5MDQyMTg2NX0.x4IxAt2lBTXsAT6pBZJyW_NL9hvzf3rUG-9EhziK7dE"
-GEMINI_KEY = "AIzaSyDq_ZItv04bA-Nt7T0ycG5Bx1Ox3PMi4lg"
+import os
+GEMINI_KEY = os.environ.get("GEMINI_KEY", "")
 TEACHER_ID = "9eb32f57-8d2b-436e-91c8-e1cd0ad9ba89"
 
 # Setup logging
